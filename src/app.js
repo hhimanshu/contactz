9cf73a93b68ff73e703ff36137d464df29b00ad9
hhimanshu <harit.himanshu@yahoo.in>
2017-09-23 10:49:24 +1200

    remove unnecessary code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routes';
@@ -11,8 +10,6 @@ app.use(logger('dev', {
   skip: () => app.get('env') === 'test'
 }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 app.use('/', routes);
