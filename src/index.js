e4b8f03b5198d2f19454610d0536f8f5c12a41fb
hhimanshu <harit.himanshu@yahoo.in>
2017-09-23 12:04:38 +1200

    Sequelize usage based on dockerized postgres database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import app from './app';
+import {Sequelize} from "sequelize";
 
 const { PORT = 8080 } = process.env;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+
+// todo: externalize (user, password, host, database)
+const sequelize = new Sequelize('postgres://postgres:contactdb@localhost:5432/contacts');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.'); // eslint-disable-line no-console
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err); // eslint-disable-line no-console
+    });
