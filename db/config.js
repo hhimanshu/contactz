5f236affbee7bb6b1beeba46155cb4de6a0a1290
hhimanshu <harit.himanshu@yahoo.in>
2017-09-26 16:24:28 +1300

    Setting up Production Database (also has set NODE_ENV in Heroku)

--- db/config.js
+++ db/config.js
@@ -1,4 +1,22 @@
+console.log("Node Env Value:", process.env);
+let productionCredentials = {
+    username: undefined,
+    password: undefined,
+    database: undefined,
+    host: undefined
+};
+
+if (process.env.DATABASE_URL) {
+    const match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+    productionCredentials.username = match[1];
+    productionCredentials.password = match[2];
+    productionCredentials.host = match[3];
+    productionCredentials.database = match[5];
+}
+
+
 module.exports = {
+
     development: {
         username: 'postgres',
         password: 'contactdb',
@@ -9,11 +27,14 @@ module.exports = {
         seederStorageTableName: "SequelizeData"
     },
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
+        username: productionCredentials.username,
+        password: productionCredentials.password,
+        database: productionCredentials.database,
+        host: productionCredentials.host,
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: true
+        },
         seederStorage: "sequelize",
         seederStorageTableName: "SequelizeData"
     }
